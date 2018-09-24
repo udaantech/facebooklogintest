54d2d1ab1a1e94f1c21f51d57c9d1b9450e96096
udaantech <amit.pandey@udaantechnologies.com>
2018-09-24 19:02:37 +0530

    first commit

---
+++ facebook.js
@@ -0,0 +1,55 @@
+var https = require('https');
+
+exports.getFbData = function(accessToken, apiPath, callback) {
+    var options = {
+        host: 'graph.facebook.com',
+        port: 443,
+        path: apiPath + '?access_token=' + accessToken, //apiPath example: '/me/friends'
+        method: 'GET'
+    };
+
+    var buffer = ''; //this buffer will be populated with the chunks of the data received from facebook
+    var request = https.get(options, function(result){
+        result.setEncoding('utf8');
+        result.on('data', function(chunk){
+            buffer += chunk;
+        });
+
+        result.on('end', function(){
+            callback(buffer);
+        });
+    });
+
+    request.on('error', function(e){
+        console.log('error from facebook.getFbData: ' + e.message)
+    });
+
+    request.end();
+}
+
+exports.getFbAccessToken = function(client_id, client_secret, redirect_uri, loginCode, callback) {
+    var options = {
+        host: 'graph.facebook.com',
+        port: 443,
+        path: '/v2.3/oauth/access_token?client_id=' + client_id + '&redirect_uri=' + redirect_uri + '/&client_secret=' + client_secret + '&code=' + loginCode,
+        method: 'GET'
+    };
+    
+    var buffer = ''; //this buffer will be populated with the chunks of the data received from facebook
+    var request = https.get(options, function(result){
+        result.setEncoding('utf8');
+        result.on('data', function(chunk){
+            buffer += chunk;
+        });
+
+        result.on('end', function(){
+            callback(buffer);
+        });
+    });
+
+    request.on('error', function(e){
+        console.log('error from facebook.getFbData: ' + e.message)
+    });
+
+    request.end();
+}
